e5d9d26195687a578f73274bf181ff5e2fb014c3
Vladislava Leonenko <aladka03@gmail.com>
2021-07-10 22:33:35 +0300

    Added some unit tests

--- UnitTestЕфыл2/UnitTestSemitrailer.cs
+++ UnitTestЕфыл2/UnitTestSemitrailer.cs
@@ -37,13 +37,13 @@ namespace UnitTestTask2
         {
             Semitrailer trailer = new Refrigerator(40, 50);
             Cargo cargo1 = new Cargo("milk", 12, 12.93, false, 3, 5, Cargo.CargoTypes.Product);
-            Cargo cargo2 = new Cargo("cream", 15.5, 16.3, false, 0, 10, Cargo.CargoTypes.Product);
+            Cargo cargo2 = new Cargo("cream", 15, 16.3, false, 0, 10, Cargo.CargoTypes.Product);
 
             trailer.LoadTrailer(cargo1);
             trailer.LoadTrailer(cargo2);
 
             string result = Convert.ToString(trailer.GetWeihgtOfAllCargo());
-            string expected = "27.5";
+            string expected = "27";
 
             Assert.AreEqual(result, expected);
         }
@@ -63,7 +63,7 @@ namespace UnitTestTask2
         }
 
         [TestMethod]
-        public void UnloadTrailer_Test()
+        public void UnloadTrailer_Test_1()
         {
             Semitrailer trailer = new Refrigerator(40, 50);
             Cargo cargo = new Cargo("milk", 12, 12.93, false, 3, 5, Cargo.CargoTypes.Product);
@@ -72,6 +72,49 @@ namespace UnitTestTask2
 
             int result = trailer.ListOfCargo.Count;
             int expected = 0;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void UnloadTrailer_Test_2()
+        {
+            Semitrailer trailer = new Refrigerator(40, 50);
+            Cargo cargo = new Cargo("milk", 12, 12.93, false, 3, 5, Cargo.CargoTypes.Product);
+            trailer.LoadTrailer(cargo);
+            trailer.UnloadTrailer(cargo, 50);
+
+            string result = Convert.ToString(trailer.GetWeihgtOfAllCargo());
+            string expected = "6";
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void JoingWithTractor_Test()
+        {
+            Semitrailer trailer = new Refrigerator(40, 50);
+            TruckTractor tractor = new TruckTractor("MAN_40.604_DFAT", 41.3, 10000);
+
+            trailer.JoingWithTractor(tractor);
+
+            string result = trailer.JoinedTractor.Model;
+            string expected = "MAN_40.604_DFAT";
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void UnhookFromTractor_Test()
+        {
+            Semitrailer trailer = new Refrigerator(40, 50);
+            TruckTractor tractor = new TruckTractor("MAN_40.604_DFAT", 41.3, 10000);
+
+            trailer.JoingWithTractor(tractor);
+            trailer.UnhookFromTractor();
+
+            TruckTractor result = trailer.JoinedTractor;
+            TruckTractor expected = null;
 
             Assert.AreEqual(result, expected);
         }
