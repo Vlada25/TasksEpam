12dd16895c253dd5a5f09a6ad49065229aed24bc
Vladislava Leonenko <aladka03@gmail.com>
2021-08-19 11:19:25 +0300

    Created class ClientMessage, implemented class MyTcpClient

---
+++ ClassLibraryTCP_IP/ClientMessage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTCP_IP
+{
+    class ClientMessage
+    {
+        public static string TranscodingMessage(string initialMessage)
+        {
+
+            StringBuilder transcodedMessage = new StringBuilder();
+
+            const int AlphabetLen = 34;
+
+            string[] rusAlphabet = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й",
+                            "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц",
+                            "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я", " " };
+
+            string[] engAlphabet = { "A", "B", "V", "G", "D", "E", "E", "ZH", "Z", "I", "Y",
+                            "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "KH", "TS",
+                            "CH", "SH", "S4", null, "Y", null, "E", "YU", "YA", " "};
+
+
+            if (Regex.IsMatch(initialMessage, @"[а-яА-Я]"))
+            {
+                for (int j = 0; j < initialMessage.Length; j++)
+                {
+                    for (int i = 0; i < AlphabetLen; i++)
+                    {
+                        if (initialMessage.Substring(j, 1) == rusAlphabet[i])
+                        {
+                            transcodedMessage.Append(engAlphabet[i]);
+                        }   
+                    }
+                }
+                    
+            }
+            else
+            {
+                for (int j = 0; j < initialMessage.Length; j++)
+                {
+                    for (int i = 0; i < AlphabetLen; i++)
+                    {
+                        if (initialMessage.Substring(j, 1) == engAlphabet[i])
+                        {
+                            string test = null;
+
+                            if (j != initialMessage.Length - 1)
+                            {
+                                test = initialMessage.Substring(j, 1) + initialMessage.Substring(j + 1, 1);
+                            }
+
+                            switch (test)
+                            {
+                                case "ZH":
+                                    transcodedMessage.Append("Ж");
+                                    break;
+                                case "KH":
+                                    transcodedMessage.Append("X");
+                                    break;
+                                case "TS":
+                                    transcodedMessage.Append("Ц");
+                                    break;
+                                case "YA":
+                                    transcodedMessage.Append("Я");
+                                    break;
+                                case "CH":
+                                    transcodedMessage.Append("Ч");
+                                    break;
+                                case "SH":
+                                    transcodedMessage.Append("Ш");
+                                    break;
+                                case "YU":
+                                    transcodedMessage.Append("Ю");
+                                    break;
+                                case "S4":
+                                    transcodedMessage.Append("Щ");
+                                    break;
+                                default:
+                                    transcodedMessage.Append(rusAlphabet[i]);
+                                    break;
+                            }
+                        }
+                    }  
+                }
+            }
+
+            return transcodedMessage.ToString();
+        }
+    }
+}
--- ClassLibraryTCP_IP/MyTcpClient.cs
+++ ClassLibraryTCP_IP/MyTcpClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,61 @@ namespace ClassLibraryTCP_IP
 {
     public class MyTcpClient
     {
+        public int Port { get; }
+        public string Ip { get; }
+        public string SenderName { get; }
+
+        public delegate string MethodContainer(string message);
+        public event MethodContainer OnTranscodingMessage;
+
+        public MyTcpClient(int port, string ip, string name)
+        {
+            Port = port;
+            Ip = ip;
+            SenderName = name;
+            OnTranscodingMessage += message => ClientMessage.TranscodingMessage(message);
+        }
+        public string ExchangeMessage(string outMessage)
+        {
+            TcpClient tcpClient = new TcpClient(Ip, Port);
+            Byte[] data = null;
+
+            if (OnTranscodingMessage != null)
+            {
+                data = Encoding.UTF8.GetBytes(SenderName + ":" + OnTranscodingMessage(outMessage));
+            }
+            else
+            {
+                data = Encoding.UTF8.GetBytes(outMessage);
+            }
+
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            try
+            {
+                networkStream.Write(data, 0, data.Length);
+
+                Byte[] readingData = new Byte[256];
+                String responseData = String.Empty;
+
+                StringBuilder completeMessage = new StringBuilder();
+                int countOfBytesRead = 0;
+
+                do
+                {
+                    countOfBytesRead = networkStream.Read(readingData, 0, readingData.Length);
+                    completeMessage.AppendFormat("{0}", Encoding.UTF8.GetString(readingData, 0, countOfBytesRead));
+                }
+                while (networkStream.DataAvailable);
+
+                responseData = completeMessage.ToString();
+                return responseData;
+            }
+            finally
+            {
+                networkStream.Close();
+                tcpClient.Close();
+            }
+        }
     }
 }
