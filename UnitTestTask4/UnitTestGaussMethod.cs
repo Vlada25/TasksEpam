32992638dad5946f0d110d6234400bb59ded3447
Vladislava Leonenko <aladka03@gmail.com>
2021-08-23 12:20:10 +0300

    Implemented class UnitTestGaussMethod()

---
+++ UnitTestTask4/UnitTestGaussMethod.cs
@@ -0,0 +1,45 @@
+﻿using ClassLibraryGauss;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace UnitTestTask4
+{
+    [TestClass]
+    public class UnitTestGaussMethod
+    {
+        [TestMethod]
+        public void MatrixKind1Solve_ReturnMultipliedSolutions()
+        {
+            int len = 2;
+            double[,] matrix = { { 1, -1, -5 },
+                                 { 2, 1, -7} };
+            MatrixKind1 matrixKind1 = new MatrixKind1(len, matrix);
+
+            Task.Run(() => { matrixKind1.Solve(matrix); });
+
+            double result = matrixKind1.Solutions[0] * matrixKind1.Solutions[1];
+
+            double expected = -4;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MatrixKind2Solve_ReturnMultipliedSolutions()
+        {
+            int len = 2;
+            double[,] matrix = { { 1, -1, -5 },
+                                 { 2, 1, -7} };
+            MatrixKind1 matrixKind2 = new MatrixKind1(len, matrix);
+
+            Task.Run(() => { matrixKind2.Solve(matrix); });
+
+            double result = matrixKind2.Solutions[0] * matrixKind2.Solutions[1];
+
+            double expected = -4;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
