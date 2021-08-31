54954b2248b9a047ae9b4df17880f0c3e598f1cf
Vladislava Leonenko <aladka03@gmail.com>
2021-08-31 17:54:01 +0300

    Added nessesary librarys

--- ExecutionTask5/Program.cs
+++ ExecutionTask5/Program.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Threading.Tasks;
 
 namespace ExecutionTask5
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("he");
+            const string DBName = "librarydb";
+            string connectionString = $"Server=localhost;Database={DBName};Trusted_Connection=True;";
+            //await Task.Run(() => CreateDB(DBName));
+
+        }
+
+        static async Task CreateDB(string name)
+        {
+            string connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            string creationString = $"CREATE DATABASE {name}";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                Console.WriteLine("Подключение открыто");
+
+                SqlCommand command = new SqlCommand();
+
+                command.CommandText = creationString;
+                command.Connection = connection;
+                await command.ExecuteNonQueryAsync();
+
+                Console.WriteLine("db was created");
+            }
+            Console.WriteLine("Подключение закрыто...");
         }
     }
 }
